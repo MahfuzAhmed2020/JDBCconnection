62cd5b442b92dc013a9d45db1e065a4c2b6676aa
Ibrahim <ibrahimmiah787@gmail.com>
2022-11-23 12:11:17 -0500

    Ibrahim

---
+++ src/main/java/com/osa/jdbcs/Test.java
@@ -0,0 +1,23 @@
+package com.osa.jdbcs;
+
+public class Test {
+
+	public static void main(String[] args) {
+
+		
+		int x=10;
+		int y= 20;
+		int sum = x+y;
+		System.out.println("Hi this is Ibrahim :"+sum);
+		
+		
+		
+		
+		
+		
+		
+		
+		
+	}
+
+}
