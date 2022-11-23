2949bb91e36a5693e2e06dfc17f073f8197225cb
mahfuz <20mahfuzahmed20@gmail.com>
2022-11-23 12:48:45 -0500

    ibrahim+Mahfuz

--- src/main/java/com/osa/jdbcs/Test.java
+++ src/main/java/com/osa/jdbcs/Test.java
@@ -8,11 +8,9 @@ public class Test {
 		int x=10;
 		int y= 20;
 		int sum = x+y;
-<<<<<<< HEAD
+
 		System.out.println("Hi this is Ibrahim+Mahfuz :"+sum);
-=======
-		System.out.println("Hi this is mahfuz :"+sum);
->>>>>>> e35f0eeeb14d0d91433bc568f47de3214bcc3194
+
 		
 		
 		
